8e153d0afa5edf35dce106a3881de20a06f2422e
Gayrat Vlasov <ggvlasov@gmail.com>
2022-05-25 09:12:50 +0300

    Кнопка «Поделиться в социальных сетях».
    Данная кнопка открывает меню с ссылками на разные социальные сети при наведении. Имеет плавную анимацию раскрытия и наведения.

---
+++ моё/Social share button/script.js
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022. Write by Gayrat
+ */
+
+// Force a hover to see the effect
+const share = document.querySelector('.share');
+
+setTimeout(() => {
+  share.classList.add("hover");
+}, 1000);
+
+setTimeout(() => {
+  share.classList.remove("hover");
+}, 3000);
